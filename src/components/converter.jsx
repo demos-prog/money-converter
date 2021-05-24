8f53524745ad05e61fe82b421008d6590aaa3b91
Дмитрий <demosne@gmail.com>
2021-05-24 16:03:42 +0300

    bn

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -5,11 +5,11 @@ import "./converter.css";
 export default function Converter() {
   // const [curValuta, setCurValuta] = useState(4);
   const [kurses, setKurses] = useState([]);
-  const [list, setList] = useState ([]);
+  const [list, setList] = useState([]);
   // Cur_ID: 292 - 5 -  EUR
   // Cur_ID: 298 - 16 - RUB
   // Cur_ID: 145 - 4 - USD
-  
+
   useEffect(() => {
     async function getKurs() {
       const url = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
@@ -17,19 +17,24 @@ export default function Converter() {
     }
 
     getKurs().then((res) => {
-      setKurses(prev => [prev, ...res]);
+      setKurses((prev) => [prev, ...res]);
     });
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     let map = kurses.map((item) => {
-      return <div key={nanoid()}>{item.Cur_OfficialRate}</div>;
+      return (
+        <div key={nanoid()}>
+          {item.Cur_Abbreviation} {item.Cur_OfficialRate}
+        </div>
+      );
     });
-    setList(map)
-  }, [kurses])
+    setList(map);
+  }, [kurses]);
 
   function handleChange(e) {
     console.log(e.target.value);
+    console.log(kurses);
   }
 
   return (
